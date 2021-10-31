451829d454125ff63226ee3cf503f0cf16157c99
Adam4531 <70113220+Adam4531@users.noreply.github.com>
2021-10-31 19:29:43 +0100

    Hangman almost done

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,16 +1,49 @@
+import java.util.Scanner;
+
 public class Hangman {
 
     private int attempts = 5;
-    private boolean isComplete = false;
+    private boolean isNotComplete = true;
+    private RandomWord word = new RandomWord();
+    private Scanner scanner = new Scanner(System.in);
+    private char lastGuess;
 
     public void run() {
         do{
-
-        } while (true);
-    }
-    boolean checkUserInput(){
-
+            getUserInput();
+            checkUserInput();
+        } while (isNotComplete);
     }
 
+    void checkUserInput(){
+        boolean isCorrect = word.addGuess(lastGuess);
 
+        if(isCorrect){
+            if(word.isComplete()){
+                System.out.println("You have won!");
+                System.out.println(word);
+            }
+        } else {
+            attempts--;
+
+            if(attempts == 0){
+                System.out.println("You have lost");
+                System.out.println(word);
+            }
+        }
+    }
+
+    void getUserInput(){
+        System.out.println("Enter your guess: ");
+        lastGuess = scanner.next().charAt(0);
+    }
+
+    void displayWord(){
+        System.out.println("Tries remaining: " + attempts);
+        System.out.println(word);
+    }
+
+    void close(){
+        scanner.close();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Hangman hangman = new Hangman();
+        hangman.run();
+        hangman.close();
     }
 }
